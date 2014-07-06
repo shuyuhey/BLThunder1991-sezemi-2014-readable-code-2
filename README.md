e3d5bb9902be69e2b649f231c465a7fe05f46582
Shuhei TAKASUGI <bd.c211@gmail.com>
2014-07-06 11:28:25 +0900

    仕様3を追加

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ gccを使用してビルドする
 
 1. % gcc recipe.c -o recipe
 2. % ./recipe
+
+## 仕様2
+このファイルを参照
+
+## 仕様3
+
+### ファイルフォーマット recipe-data.txt
+   % cat recipe-data.txt
+
+   オムライス
+
+### プログラムのコンパイルと実行
+   % gcc recipe.c -o recipe
+
+   % ./recipe recipe-data.txt
+
+   オムライス
--- recipe.c
+++ recipe.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 
-int main (void){
-  printf ("オムライス\n");
+int main (int argc, char* argv[]){
+  FILE *recipe_fp;
+
+  /* エラー処理 */
+  if (argc < 2){
+    fprintf (stderr, "Usage:recipe [file_name]\n");
+    return -1;
+  }
+  char *recipe_file_path = argv[1];
+  if ((recipe_fp = fopen(recipe_file_path , "r")) == NULL){
+    fprintf (stderr, "ファイルが開けません\n");
+    return -1;
+  }
+
+  /* 全てのレシピを出力 */
+  char recipe_line[256];
+  while (fscanf (recipe_fp, "%s", recipe_line) != EOF){
+    printf ("%s\n", recipe_line);
+  }
+
   return 0;
 }
