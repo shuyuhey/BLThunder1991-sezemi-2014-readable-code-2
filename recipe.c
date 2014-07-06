23eb0cfe1b8814b21b46329c55b9c52234fbfd0c
Shuhei TAKASUGI <bd.c211@gmail.com>
2014-07-06 11:58:32 +0900

    またまたコードのリファクタリング

--- recipe.c
+++ recipe.c
@@ -6,25 +6,25 @@ int main (int argc, char* argv[]){
 
   /* Usageの表示 */
   if (argc < 2){
-    fprintf (stderr, "Usage:recipe [file_name]\n");
+    fprintf ( stderr, "Usage:recipe [file_name]\n");
     return -1;
   }
 
   /* ファイルを開く */
   char *recipe_file_path = argv[1];
-  if ((recipe_fp = fopen(recipe_file_path , "r")) == NULL){
-    fprintf (stderr, "ファイルが開けません\n");
+  if ( ( recipe_fp = fopen( recipe_file_path , "r")) == NULL){
+    fprintf ( stderr, "ファイルが開けません\n");
     return -1;
   }
 
   /* 全てのレシピを出力 */
   char recipe_line[256];
   int recipe_id = 1;
-  while (fscanf (recipe_fp, "%s", recipe_line) != EOF){
+  while ( fscanf (recipe_fp, "%s", recipe_line) != EOF){
 
-    if( strlen(recipe_line) == 0) continue;
+    if ( strlen(recipe_line) == 0) continue;
 
-    printf ("%d: %s\n", recipe_id, recipe_line);
+    printf ( "%d: %s\n", recipe_id, recipe_line);
     recipe_id++;
   }
 
